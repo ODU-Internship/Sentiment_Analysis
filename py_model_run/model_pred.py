e5fde79ca8a5490b2e8583e8de4713509dc5ca73
M Aishwarya <37695924+Quartz14@users.noreply.github.com>
2021-05-12 16:14:33 +0530

    fixed issue due to change in variable name
    made tokenizer global
    changed pred_sentence to text_list variable

--- py_model_run/model_pred.py
+++ py_model_run/model_pred.py
@@ -4,7 +4,9 @@ from transformers import InputExample, InputFeatures
 import tensorflow as tf
 import pandas as pd
 
-def load_model(checkpoint_path = "training_1/cp.ckpt"):
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+def load_model(checkpoint_path = "training_1/cp.ckpt", tokenizer=tokenizer):
     model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased",num_labels=5)
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
     model_reloaded = TFBertForSequenceClassification.from_pretrained("bert-base-uncased",num_labels=5)
@@ -12,8 +14,8 @@ def load_model(checkpoint_path = "training_1/cp.ckpt"):
     model_reloaded.load_weights(checkpoint_path)
     return model_reloaded
 
-def get_predictions(text_list):
-    tf_batch = tokenizer(pred_sentences, max_length=500, padding=True, truncation=True, return_tensors='tf')
+def get_predictions(text_list,model_reloaded,tokenizer=tokenizer):
+    tf_batch = tokenizer(text_list, max_length=500, padding=True, truncation=True, return_tensors='tf')
     tf_outputs = model_reloaded(tf_batch)
     tf_predictions = tf.nn.softmax(tf_outputs[0], axis=-1)
     label = tf.argmax(tf_predictions, axis=1)
@@ -21,10 +23,11 @@ def get_predictions(text_list):
     labels = ['terrible','bad','neutral','happy','very happy']
     sentiment_map = {0:'negative', 1:'negative', 2:'neutral', 3:'positive' ,4:'positive' }
     predictions = {} #format: text:[sentiment, categorization]
-    for i in range(len(pred_sentences)):
-        predictions[pred_sentences[i]] = [sentiment_map[label[i]],labels[label[i]]]
+    for i in range(len(text_list)):
+        predictions[text_list[i]] = [sentiment_map[label[i]],labels[label[i]]]
 
     return predictions
+
 
 
 model = load_model()
